70caa90eef238c1152a700ba2f784ca170a312b4
evridiki91 <ec15441@my.bristol.ac.uk>
2018-04-05 05:08:48 +0100

    Minor changes to interface

--- faceRecognition/faceRecognition/PageViewController.swift
+++ faceRecognition/faceRecognition/PageViewController.swift
@@ -13,12 +13,14 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
     lazy var viewControllerList: [UIViewController] = {
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         
+        let vc0 = storyBoard.instantiateViewController(withIdentifier: "zeroVC")
         let vc1 = storyBoard.instantiateViewController(withIdentifier: "firstVC")
         let vc2 = storyBoard.instantiateViewController(withIdentifier: "secondVC")
         let vc3 = storyBoard.instantiateViewController(withIdentifier: "thirdVC")
         let vc4 = storyBoard.instantiateViewController(withIdentifier: "fourthVC")
+        let vc5 = storyBoard.instantiateViewController(withIdentifier: "fifthVC")
         
-        return [vc1,vc2,vc3,vc4]
+        return [vc0,vc1,vc2,vc3,vc4,vc5]
     }()
     
     override func viewDidLoad() {
